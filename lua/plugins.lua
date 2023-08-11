17967b2493f323b67cf77703ff4096ed22b99224
nakao keisuke <r3grashopper193@gmail.com>
2023-08-12 08:06:16 +0900

    set lsp config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,4 +17,20 @@ return require('packer').startup(function(use)
  			'nvim-lualine/lualine.nvim',
   		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 	}
+
+	-- lsp
+	-- refer to the following
+	-- https://zenn.dev/fukakusa_kadoma/articles/99e8f3ab855a56
+	use {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+	}
+	use 'hrsh7th/nvim-cmp' --補完エンジン本体
+  use 'hrsh7th/cmp-nvim-lsp' --LSPを補完ソースに
+  use 'hrsh7th/cmp-buffer' --bufferを補完ソースに
+  use 'hrsh7th/cmp-path' --pathを補完ソースに
+  use 'hrsh7th/vim-vsnip' --スニペットエンジン
+  use 'hrsh7th/cmp-vsnip' --スニペットを補完ソースに
+  use 'onsails/lspkind.nvim' --補完欄にアイコンを表示
 end)
